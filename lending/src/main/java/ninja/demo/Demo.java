127ac1c33f50f6ae36ad2d390c02e61fc8416aea
Ninja Developer <info@pegadevops.com>
2018-11-20 11:15:46 +0300

    RD-387 Update Demo project Default cookie mode is set to DEV, BULK is used in build-server profile.

---
+++ lending/src/main/java/ninja/demo/Demo.java
@@ -0,0 +1,21 @@
+package ninja.demo;
+
+@SuppressWarnings({"unused", "NonAsciiCharacters"})
+public interface Demo {
+    //Class
+    String C_NINJA_DEMO = "Ninja-Demo";
+
+    // Rule-Declare-DecisionTable
+    String R_DECISION_TABLE_CALCULATE_RISK_CATEGORY_NJA = "CalculateRiskCategory";
+
+    // Rule-Obj-Property
+    String P_PROPERTY_AGE_NJA = "Age";
+    String P_PROPERTY_AMOUNT_NJA = "Amount";
+    String P_PROPERTY_CASE_TO_LAUNCH_NJA = "CaseToLaunch";
+    String P_PROPERTY_CURRENCY_NJA = "Currency";
+    String P_PROPERTY_REQUEST_CATEGORY_NJA = "RequestCategory";
+
+    // Rule-Obj-When
+    String R_WHEN_UPGRADE_PLAN_NJA = "UpgradePlan";
+
+}
